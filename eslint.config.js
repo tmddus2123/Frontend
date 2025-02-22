3f3c95021dc6636c5dd72039fdeadb925a20852e
HyebinHwang <sksp0401@gmail.com>
2025-02-22 14:35:54 +0900

    Fix: lint 수정

--- eslint.config.js
+++ eslint.config.js
@@ -3,52 +3,37 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import lintJsxA11y from 'eslint-plugin-jsx-a11y';
 import lintReact from 'eslint-plugin-react';
 import lintImport from 'eslint-plugin-no-relative-import-paths';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, , ...tseslint.configs.recommended],
+    ...lintReact.configs.flat.recommended,
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-import': lintImport,
-      'react-lint': lintReact,
-      'react-recommended': lintJsxA11y,
+      'no-relative-import-paths': lintImport,
+      'jsx-a11y': jsxA11y,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      // <img> 엘리먼트에 유의미한 대체 텍스트가 있는지 체크
+      'no-console': ['error'],
+      'no-relative-import-paths/no-relative-import-paths': 'error',
       'jsx-a11y/alt-text': [
         'warn',
         {
           elements: ['img'],
         },
       ],
-      // 유효한 aria-* 속성만 사용
-      'jsx-a11y/aria-props': 'warn',
-      // 유효한 aria-* 상태/값만 사용
-      'jsx-a11y/aria-proptypes': 'warn',
-      // DOM에서 지원되는 role, ARIA만 사용
-      'jsx-a11y/aria-unsupported-elements': 'warn',
-      // 필수 ARIA 속성이 빠져있는지 체크
-      'jsx-a11y/role-has-required-aria-props': 'warn',
-      // ARIA 속성은 지원되는 role에서만 사용
-      'jsx-a11y/role-supports-aria-props': 'warn',
-      // DOM에 정의되지 않은 속성을 사용했는지 체크 (emotion css 속성 등 예외 케이스가 있으므로 기본은 off)
       'react/no-unknown-property': 'off',
-      // 정의한 props 중에 빠진게 있는지 체크 (NextPage 등 일부 추상화 컴포넌트에서 복잡해지므로 기본은 off)
       'react/prop-types': 'off',
       'react-refresh/only-export-components': [
         'warn',
