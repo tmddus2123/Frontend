c3565689634306c6aa844c57194f9091759965cc
HyebinHwang <sksp0401@gmail.com>
2025-02-22 13:34:03 +0900

    Fix: prettier eslint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,25 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import lintJsxA11y from 'eslint-plugin-jsx-a11y';
+import lintReact from 'eslint-plugin-react';
+import lintImport from 'eslint-plugin-no-relative-import-paths';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, , ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
+      'react-import': lintImport,
+      'react-lint': lintReact,
+      'react-recommended': lintJsxA11y,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
@@ -24,5 +30,5 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  },
-)
+  }
+);
