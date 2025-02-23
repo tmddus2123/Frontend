da36a6608b59a8d52363d9e7670ce4011a96e96d
HyebinHwang <sksp0401@gmail.com>
2025-02-23 15:14:29 +0900

    Feature: msw init

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'src/index.css';
+import App from 'src/App.tsx';
+
+async function init() {
+  if (process.env.NODE_ENV === 'development') {
+    const { worker } = await import('./mocks/browser.ts');
+
+    return worker.start();
+  }
+}
+await init();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
