253b9b244b7d2434095c90c448cb2162375ee05b
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-02-26 23:53:20 +0900

    feat: 로딩 화면 ui

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,90 @@
+import LoadingChat from '@/assets/images/loading.svg?react';
+import styled, { keyframes } from 'styled-components';
+
+interface LoadingProps {
+  first: string;
+  second: string;
+  detail: string;
+}
+
+const Loading = ({ first, second, detail }: LoadingProps) => {
+  return (
+    <Container>
+      <LoadingWrapper>
+        <LoadingChat />
+        <Circle />
+      </LoadingWrapper>
+      <LabelWrapper>
+        <Title>
+          {first}
+          <br />
+          {second}
+        </Title>
+        <Detail>{detail}</Detail>
+      </LabelWrapper>
+    </Container>
+  );
+};
+
+export default Loading;
+
+const Container = styled.div`
+  background: ${({ theme }) => theme.colors['primary-500']};
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 32px;
+`;
+
+const LoadingWrapper = styled.div`
+  position: relative;
+  width: 114.589px;
+  height: 114.589px;
+`;
+
+const rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const Circle = styled.div`
+  position: absolute;
+  width: 72px;
+  height: 72px;
+  border: 10px solid transparent;
+  border-top: 10px solid ${({ theme }) => theme.colors['primary-500']};
+  border-radius: 50%;
+  top: 18%;
+  left: 18%;
+  animation: ${rotate} 2s linear infinite;
+`;
+
+const LabelWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`;
+
+const Title = styled.label`
+  font-family: 'GmarketSansMedium';
+  font-size: 26px;
+  line-height: 34px;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+const Detail = styled.label`
+  font-family: 'Pretendard Variable';
+  font-size: 18px;
+  font-weight: 500;
+  text-align: center;
+  color: ${({ theme }) => theme.colors['primary-200']};
+`;
