05603e58141788a135ef25095b233a8141b05f01
HyebinHwang <sksp0401@gmail.com>
2025-02-28 00:09:49 +0900

    Feat: Button 컴포넌트 적용

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,30 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+import styled from 'styled-components';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  colorScheme?: 'primary' | 'white';
+}
+
+export default function Button({ children, ...props }: ButtonProps) {
+  return <StyledButton {...props}>{children}</StyledButton>;
+}
+
+const StyledButton = styled.button<ButtonProps>`
+  background-color: ${({ theme, colorScheme }) =>
+    colorScheme === 'white'
+      ? theme.colors['white']
+      : theme.colors['primary-500']};
+  font-family: 'Pretendard Variable';
+  border-radius: 12px;
+  padding: 12px 0;
+  color: ${({ theme, colorScheme }) =>
+    colorScheme === 'white' ? theme.colors['gray-900'] : theme.colors['white']};
+  width: 100%;
+  font-weight: bold;
+
+  &:disabled {
+    background: ${({ theme }) => theme.colors['gray-200']};
+    color: ${({ theme }) => theme.colors['gray-500']};
+  }
+`;
